3a3339268bddf860695eeefdab5764f3d67f89d1
Joshua Welsh <j22yosh@gmail.com>
2022-07-29 18:27:22 -0400

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,47 @@
+
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+  def initialize (a, b ,c)
+    @a = a
+    @b = b
+    @c = c
+       
+  end
+  
+  def kind
+  if validate_triangle?
+    if a==b && b==c && a==c
+      :equilateral
+    elsif a == b || a== c || b == c
+      :isosceles
+    else 
+      :scalene
+    end
+  else
+      begin
+        raise TriangleError
+      # rescue TriangleError => error
+      #   puts error.message
+      end
+    end
+  end
+
+
+  class TriangleError < StandardError
+    # triangle error code
+    def message
+      puts "Invalid Triangle"
+    end
+  end
+
+def triangle_inequality?
+  a+b>c && b+c>a && a+c>b
 end
+
+def validate_triangle?
+  [a,b,c].all?(&:positive?) && triangle_inequality?
+end
+
+end
+
